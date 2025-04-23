23063aa7da87701f68f2a257bb84aa9455822f70
Dinuka Liyanage <infodinukalk@gmail.com>
2025-04-23 18:03:31 +0530

    add new role function

--- routes/RoleRoute.js
+++ routes/RoleRoute.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const RoleController = require('../controller/RoleController');
 
-//implementing the routes
+
+router.post('/create-role',RoleController.createRole);
 
 
 module.exports = router
